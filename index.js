0bef5ab86edc03ed9f3a15f3026afdd9f2fe1322
adiatma <adiatma9024@gmail.com>
2017-11-04 18:52:38 +0800

    MODIFIED: new style

---
+++ index.js
@@ -0,0 +1,1 @@
+module.exports = reqiure('./src/js/index.js');
---
+++ src/js/simple-modal.js
@@ -0,0 +1,89 @@
+/**
+* Simple Modal
+* Author adiatma9024@gmail.com
+*/
+
+;(function(root, factory) {
+  root.SimpleModal = factory();
+})(this, function () {
+
+  /**
+  * write with es2015
+  */
+
+  function el(selector) {
+    return document.querySelector(selector);
+  }
+
+  function findChildren(element, selector) {
+    return element.querySelectorAll(selector);
+  }
+
+  function createModalContent() {
+    var modalContent = document.createElement('div');
+    modalContent.classList.add('modal-content');
+    return modalContent;
+  }
+
+  function createSpanClose() {
+    var spanClose = document.createElement('span');
+    spanClose.classList.add('close');
+    spanClose.innerHTML = '&times;';
+    return spanClose;
+  }
+
+  function configModal(options) {
+  
+    this.options = Object.assign({}, options);
+
+    this.width = this.options.width || 400;
+
+    this.wrapModalContent = this.wrapModalContent.bind(this);
+  
+    this.makeModal();
+  
+  };
+
+  configModal.prototype.makeModal = function makeModal() {
+    
+    // Add class modal
+    var modal = el(this.options.modal);
+    modal.classList.add('modal');
+    
+    // Add modal content
+    var content = findChildren(modal, this.options.content);
+    var modalContent = this.wrapModalContent(content);
+
+    // Add action close 
+    var close = findChildren(modalContent, '.close')[0];
+    close.addEventListener('click', function() {
+      modal.style.display = 'none';
+    });
+    
+    // Add trigger action
+    var trigger = el(this.options.trigger);
+    trigger.addEventListener('click', function() {
+      modal.style.display = 'block';
+    }); 
+
+    modal.appendChild(modalContent);
+  
+  }
+
+  configModal.prototype.wrapModalContent = function wrapModalContent(children) {
+    var fragmentModalContent = document.createDocumentFragment();
+    var modalContent = createModalContent();
+    modalContent.style.width = this.width + 'px';
+
+    Array.prototype.forEach.call(children, function(e) {  
+      modalContent.appendChild(createSpanClose());
+      modalContent.appendChild(e);
+      fragmentModalContent.appendChild(modalContent);
+    });
+
+    return fragmentModalContent;
+  }
+
+  return configModal;
+
+});
